806e9fd9125278eb56c2c89bf4795efb04aa9340
Sagar Dholariya <148209811+sagardholariya18@users.noreply.github.com>
2024-05-14 13:53:02 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+# import qrcode as qr
+
+
+# img = qr.make("https://www.youtube.com/results?search_query=surat+gurukul")
+
+# img.save("hello.png")
+
+import qrcode as qr
+
+img = qr.make("https://www.transglobeedu.com/")
+img.save("tg.png")
---
+++ sagar.py
@@ -0,0 +1,27 @@
+# # class demo:
+# #     def config(self):
+# #         print("hello")
+# # a = 8
+# # print(type(a))
+# # hello = demo()
+# # print(type(hello))
+
+
+
+# class com:     
+#     def config(self):
+#         for i in "hello":
+#             print(i)
+# com1 = com()
+# # com2 = com()
+
+# com.config(com1)
+# # com.config(com2)
+
+num = int(input("enter the number: "))
+for i in range(2,num):
+    if num % i == 0:
+        print("not peime number")
+        break
+    else:
+        print("prime number")
